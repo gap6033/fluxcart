18a753da3bfee6b3a564b7d8a1a089cfa7be9773
Gaurang Pateriya <gap6033@gmail.com>
2024-05-27 13:53:18 +0530

    added validate method to ContactSerializer

--- bitespeed/serializers.py
+++ bitespeed/serializers.py
@@ -7,5 +7,10 @@ class ContactSeriazlizer(serializers.ModelSerializer):
         model = Contact
         fields = ['email', 'phoneNumber']
 
+    def validate(self, data):
+        if not data['email'] and not data['phoneNumber']:
+            raise serializers.ValidationError("At least email or phoneNumber has to be provided")
+        return data
+
     def create(self, validated_data):
         return ContactService.create_contact(validated_data)
