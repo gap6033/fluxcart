9cec40adca467054fe16c51afb5ed25678ce5a29
Gaurang Pateriya <gap6033@gmail.com>
2024-05-27 13:46:03 +0530

    added custom create method to ContactSerializer

--- bitespeed/serializers.py
+++ bitespeed/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from .models import Contact
+from .services import ContactService
 
 class ContactSeriazlizer(serializers.ModelSerializer):
     class Meta:
         model = Contact
         fields = ['email', 'phoneNumber']
+
+    def create(self, validated_data):
+        return ContactService.create_contact(validated_data)
