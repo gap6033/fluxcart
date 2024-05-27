233803ea800e562ade9cf125b764713b46a430b6
Gaurang Pateriya <gap6033@gmail.com>
2024-05-27 14:01:44 +0530

    added migrations

--- bitespeed/views.py
+++ bitespeed/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from .serializers import ContactSeriazlizer
+from rest_framework.response import Response
+from rest_framework import status
+from .services import ContactService
 
 # Create your views here.
 @api_view(['POST'])
 def identify(request):
-    pass
+    serializer = ContactSeriazlizer(data=request.data)
+    if not serializer.is_valid():
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    instance = serializer.save()
+    return Response(ContactService.get_all_connected_contacts(instance), status=status.HTTP_200_OK)
+
+
