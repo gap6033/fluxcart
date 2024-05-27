c029a0bf7776df9b0e52dd6b8ec4b2669e1cd4b7
Gaurang Pateriya <gap6033@gmail.com>
2024-05-27 12:09:06 +0530

    created contact data model

--- bitespeed/models.py
+++ bitespeed/models.py
@@ -1,3 +1,20 @@
+# Create your models here.
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    PRIMARY = 'primary'
+    SECONDARY = 'secondary'
+
+    CONTACT_TYPE = [
+        (PRIMARY, 'primary'),
+        (SECONDARY, 'secondary')
+    ]
+
+    email = models.EmailField(blank=True, null=True, db_index=True)
+    phone_number = models.CharField(max_length=30, blank=True, null=True, db_index=True)
+    linkedId = models.IntegerField(null=True)
+    linkPrecedence = models.CharField(max_length=256, choices=CONTACT_TYPE, default=PRIMARY)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(auto_now_add=True, null=True)
