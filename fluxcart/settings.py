de7c36da11c2876da3961f53cfd24370fd6b846e
Gaurang Pateriya <gap6033@gmail.com>
2024-05-27 16:15:37 +0530

    updated Debug to False

--- fluxcart/settings.py
+++ fluxcart/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&(=1+cq_+@5!po)rp-3*5o&(v!$ewf-r3^g(&h=@n(to3_qfh-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['fluxcart-eesk.onrender.com', 'localhost', '127.0.0.1']
 
