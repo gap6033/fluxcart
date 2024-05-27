871448abec894f02714aa2944a9721717c233305
Gaurang Pateriya <gap6033@gmail.com>
2024-05-27 13:41:24 +0530

    added services.py to handle business logic for contact creation

---
+++ bitespeed/services.py
@@ -0,0 +1,57 @@
+from .models import Contact
+
+class ContactService:
+
+    @staticmethod
+    def create_contact(validated_data: dict):
+        email = validated_data.get('email')
+        phone = validated_data.get('phoneNumber')
+
+        primary_contact = None
+        contact_by_email = None
+        contact_by_phone = None
+        if email:
+            contact_by_email = Contact.objects.filter(email = email).first()
+        if phone:
+            contact_by_phone = Contact.objects.filter(phone = phone).first()
+
+        if not contact_by_email and not contact_by_phone:
+            primary_contact = Contact.objects.create(email = email, phone = phone)
+        elif not contact_by_phone:
+            primary_contact_by_email = ContactService.get_primary_contact(contact_by_email)
+            Contact.objects.create(email = email, phone = phone, linkedId = primary_contact_by_email.id, linkedPreference = Contact.SECONDARY)
+            primary_contact = primary_contact_by_phone
+        elif not contact_by_email:
+            primary_contact_by_phone = ContactService.get_primary_contact(contact_by_phone)
+            Contact.objects.create(email = email, phone = phone, linkedId = primary_contact_by_phone.id, linkedPreference = Contact.SECONDARY)
+            primary_contact = primary_contact_by_phone
+        else:
+            primary_contact_by_email = ContactService.get_primary_contact(contact_by_email)
+            primary_contact_by_phone = ContactService.get_primary_contact(contact_by_phone)
+            if primary_contact_by_phone != primary_contact_by_email:
+                if primary_contact_by_phone.createdAt < primary_contact_by_email.createdAt:
+                    primary_contact = ContactService.update_primary_to_secondary(primary_contact_by_phone, primary_contact_by_email)
+                elif primary_contact_by_phone.createdAt > primary_contact_by_email.createdAt:
+                    primary_contact = ContactService.update_primary_to_secondary(primary_contact_by_email, primary_contact_by_phone)
+            else:
+                primary_contact = primary_contact_by_email
+        return primary_contact
+        
+    @staticmethod
+    def get_primary_contact(curr_contact: Contact):
+        if curr_contact.linkPrecedence != Contact.PRIMARY:
+            primary_contact = Contact.objects.get(id = curr_contact.linkedId)
+        else:
+            primary_contact = curr_contact
+        return primary_contact
+    
+    @staticmethod
+    def update_primary_to_secondary(primary_contact: Contact, secondary_contact: Contact):
+        secondary_contact.linkPrecedence = Contact.SECONDARY
+        secondary_contact.linkedId = primary_contact.id
+        secondary_contact.save()
+        contacts = Contact.objects.filter(linkedId = secondary_contact.id)
+        for contact in contacts:
+            contact.linkedId = primary_contact.id
+            contact.save()
+            
--- bitespeed/views.py
+++ bitespeed/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from .serializers import ContactSeriazlizer
 
 # Create your views here.
 @api_view(['POST'])
