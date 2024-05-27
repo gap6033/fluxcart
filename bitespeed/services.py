5846a821f5a188278990d546a8b1d4d4c606e87c
Gaurang Pateriya <gap6033@gmail.com>
2024-05-27 17:19:00 +0530

    added docstring to services.py methods

--- bitespeed/services.py
+++ bitespeed/services.py
@@ -5,7 +5,15 @@ class ContactService:
 
     @staticmethod
     @transaction.atomic
-    def create_contact(validated_data: dict):
+    def create_contact(validated_data: dict) -> Contact:
+        """Handle contact creation in database based on provided input
+
+        Args:
+            validated_data (dict): the input received via post request
+
+        Returns:
+            Contact: the primary contact
+        """
         email = validated_data.get('email')
         phone = validated_data.get('phoneNumber')
 
@@ -15,26 +23,34 @@ class ContactService:
         if email:
             contact_by_email = Contact.objects.filter(email = email).first()
             if contact_by_email and not phone:
+                # email exists in database but no phone provided in input
                 return ContactService.get_primary_contact(contact_by_email)
+            
         if phone:
             contact_by_phone = Contact.objects.filter(phoneNumber = phone).first()
             if contact_by_phone and not email:
+                # phone exists in database but no email provided in input
                 return ContactService.get_primary_contact(contact_by_phone)
 
+        # create contact if both email and phone not present in database
         if not contact_by_email and not contact_by_phone:
             primary_contact = Contact.objects.create(email = email, phoneNumber = phone)
         elif not contact_by_phone:
+            # add phone to database when email already exists
             primary_contact_by_email = ContactService.get_primary_contact(contact_by_email)
             Contact.objects.create(email = email, phoneNumber = phone, linkedId = primary_contact_by_email.id, linkPrecedence = Contact.SECONDARY)
             primary_contact = primary_contact_by_email
         elif not contact_by_email:
+            # add email to database when phone already exists
             primary_contact_by_phone = ContactService.get_primary_contact(contact_by_phone)
             Contact.objects.create(email = email, phoneNumber = phone, linkedId = primary_contact_by_phone.id, linkPrecedence = Contact.SECONDARY)
             primary_contact = primary_contact_by_phone
         else:
+            #both phone and email exists
             primary_contact_by_email = ContactService.get_primary_contact(contact_by_email)
             primary_contact_by_phone = ContactService.get_primary_contact(contact_by_phone)
             if primary_contact_by_phone != primary_contact_by_email:
+                #if the primary contact of phone and email is different
                 if primary_contact_by_phone.createdAt < primary_contact_by_email.createdAt:
                     primary_contact = ContactService.update_primary_to_secondary(primary_contact_by_phone, primary_contact_by_email)
                 elif primary_contact_by_phone.createdAt > primary_contact_by_email.createdAt:
@@ -44,7 +60,15 @@ class ContactService:
         return primary_contact
         
     @staticmethod
-    def get_primary_contact(curr_contact: Contact):
+    def get_primary_contact(curr_contact: Contact) -> Contact:
+        """Get primary contact from the provided contact
+
+        Args:
+            curr_contact (Contact): the contact provided as input
+
+        Returns:
+            Contact: the primary contact
+        """
         if curr_contact.linkPrecedence != Contact.PRIMARY:
             primary_contact = Contact.objects.get(id = curr_contact.linkedId)
         else:
@@ -52,7 +76,17 @@ class ContactService:
         return primary_contact
     
     @staticmethod
-    def update_primary_to_secondary(primary_contact: Contact, secondary_contact: Contact):
+    def update_primary_to_secondary(primary_contact: Contact, secondary_contact: Contact) -> Contact:
+        """Update a primary contact to secondary and updates all its corresponding secondary contacts to point
+        to new linkedId
+
+        Args:
+            primary_contact (Contact): the primary contact_
+            secondary_contact (Contact): the primary contact that is to be converted to secondary
+
+        Returns:
+            Contact: The primary contact
+        """
         secondary_contact.linkPrecedence = Contact.SECONDARY
         secondary_contact.linkedId = primary_contact.id
         secondary_contact.save()
@@ -63,7 +97,15 @@ class ContactService:
         return primary_contact
     
     @staticmethod
-    def get_all_connected_contacts(primary_contact: Contact):
+    def get_all_connected_contacts(primary_contact: Contact) -> dict:
+        """Build the output for response
+
+        Args:
+            primary_contact (Contact): The primary contact
+
+        Returns:
+            dict: the output containing all the connected contacts from a given primary contact
+        """
         secondary_contacts = Contact.objects.filter(linkedId = primary_contact.id)
         emails = set()
         if primary_contact.email:
