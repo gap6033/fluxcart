dda0810e09d90a2752aba1ad906bef8d1b6a32fe
Gaurang Pateriya <gap6033@gmail.com>
2024-06-07 16:57:57 +0530

    updated identify view to provide response in case of GET request

--- bitespeed/views.py
+++ bitespeed/views.py
@@ -3,10 +3,17 @@ from .serializers import ContactSeriazlizer
 from rest_framework.response import Response
 from rest_framework import status
 from .services import ContactService
+from django.http import JsonResponse
 
 # Create your views here.
-@api_view(['POST'])
+@api_view(['GET', 'POST'])
 def identify(request):
+    if request.method == 'GET':
+        if 'text/html' in request.META.get('HTTP_ACCEPT', ''):
+            print(request.META)
+            return JsonResponse({'error': 'Method Not Allowed'})
+        else:
+            return Response({'error': 'Method Not Allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
     serializer = ContactSeriazlizer(data=request.data)
     if not serializer.is_valid():
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
